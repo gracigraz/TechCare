3ae26b3fb8f0bb6bacf7199472bb5cb7b3c77742
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2024-05-26 19:36:44 -0400

    added PatientProfile component with static data and styling

---
+++ src/components/PatientProfile/PatientProfile.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./PatientProfile.scss";
+import birthday from "../../assets/icons/birthIcon.svg";
+import female from "../../assets/icons/femaleIcon.svg"
+import phone from "../../assets/icons/phoneIcon.svg"
+import insurance from "../../assets/icons/insuranceIcon.svg"
+import jessica from "../../assets/images/Jessica.png"
+
+interface Patient {
+    name: string;
+    gender: string;
+    profile_picture: string;
+    age: number;
+    date_of_birth: string;
+    phone_number: string;
+    emergency_contact: string;
+    insurance_type: string;
+  }
+  
+  interface PatientProfileProps {
+    patient: Patient;
+  }
+
+const PatientProfile: React.FC<PatientProfileProps> = ({ patient}) => {
+	return (
+		<div className="patient-profile">
+			<img className="patient-profile-img" src={jessica} alt="Patient profile" />
+			<h1 className="patient-profile__name">{patient.name}</h1>
+			<ul className="patient-profile__list">
+				<li className="patient-profile__item">
+					<img className="patient-profile__icon" src={birthday} alt="calendar" />
+                    <div className="patient-profile__content">
+                        <p className="patient-profile__text">Date Of Birth</p>
+                        <p className="patient-profile__text patient-profile__text--bold">{patient.date_of_birth}</p>
+                    </div>
+				</li>
+                <li className="patient-profile__item">
+					<img className="patient-profile__icon" src={female} alt="female/male symbol" />
+                    <div className="patient-profile__content">
+                        <p className="patient-profile__text">Gender</p>
+                        <p  className="patient-profile__text patient-profile__text--bold">{patient.gender}</p>
+                    </div>
+				</li>
+                <li className="patient-profile__item">
+					<img className="patient-profile__icon" src={phone} alt="phone" />
+                    <div className="patient-profile__content">
+                        <p className="patient-profile__text">Contact Info.</p>
+                        <p  className="patient-profile__text patient-profile__text--bold">{patient.phone_number}</p>
+                    </div>
+				</li>
+                <li className="patient-profile__item">
+					<img  className="patient-profile__icon" src={phone} alt="phone" />
+                    <div className="patient-profile__content">
+                        <p className="patient-profile__text">Emergency Contacts</p>
+                        <p  className="patient-profile__text patient-profile__text--bold">{patient.emergency_contact}</p>
+                    </div>
+				</li>
+                <li className="patient-profile__item">
+					<img className="patient-profile__icon" src={insurance} alt="insurance badge" />
+                    <div className="patient-profile__content">
+                        <p className="patient-profile__text">Insurance Provider</p>
+                        <p  className="patient-profile__text patient-profile__text--bold">{patient.insurance_type}</p>
+                    </div>
+				</li>
+			</ul>
+            <button className="patient-profile__button">Show All Information</button>
+		</div>
+	);
+};
+
+export default PatientProfile;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./HomePage.scss";
 import Header from "../../components/Header/Header";
 import Patients from "../../components/Patients/Patients";
+import PatientProfile from "../../components/PatientProfile/PatientProfile";
+
 import lady from "../../assets/images/lady.png";
 
 const Home: React.FC = () => {
+    // const [patient, setPatient] = useState<Patient | null>(null);
+
+    // useEffect(() => {
+    //   fetchPatientData();
+    // }, []);
     return (
 		<>
-			<main className="home">
-                <Header/>
-                <Patients patients={[
+            <Header/>
+            <main className="home">
+            <Patients patients={[
                     {name: "John Doe", profile_picture: lady, gender: "Female", age:18} , {name: "Jane Doe", profile_picture: lady, gender: "Female", age:18},  { name: "John Smith", profile_picture: lady, gender: "Female", age:21 }    ]}/>
+                
+            <section className="home__diagnosis-info">
+               </section>
+               <section className="home__patient-info">
+               <PatientProfile patient={{name: "Jessica Taylor",
+                                gender: "Female",
+                                age: 28,
+                                profile_picture: "https://fedskillstest.ct.digital/4.png",
+                                date_of_birth: "1996-08-23",
+                                phone_number: "(415) 555-1234",
+                                emergency_contact: "(415) 555-5678",
+                                insurance_type: "Sunrise Health Assurance"}}/>
+               </section>
+            
             </main>
             </>
 	);
